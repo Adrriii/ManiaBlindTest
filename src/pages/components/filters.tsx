54cb68475dd429c8b3d4e7b62a231b94432e103f
Adri <adrien.boitelle@gmail.com>
2023-03-26 11:35:33 +0200

    other filters

--- src/pages/components/filters.tsx
+++ src/pages/components/filters.tsx
@@ -12,6 +12,34 @@ export default function Filters() {
 		setOpen(!open);
 	}
 
+	const optionsDiffMin = [
+		{ value: 'lowest', label: 'Diff Min'}
+	];
+	for(let d = 1; d <= 10; d++) {
+		optionsDiffMin.push({ value: `${d}`, label: `${d}*` });
+	}
+
+	const optionsDiffMax = [
+		{ value: 'highest', label: 'Diff Max'}
+	];
+	for(let d = 1; d <= 10; d++) {
+		optionsDiffMax.push({ value: `${d}`, label: `${d}*` });
+	}
+
+	const optionsYearMin = [
+		{ value: 'start', label: 'Start year'}
+	];
+	for(let d = 2014; d <= 2023; d++) {
+		optionsYearMin.push({ value: `${d}`, label: `${d}` });
+	}
+
+	const optionsYearMax = [
+		{ value: 'now', label: 'End year'}
+	];
+	for(let d = 2014; d <= 2023; d++) {
+		optionsYearMax.push({ value: `${d}`, label: `${d}` });
+	}
+
 	return (<>
 		<div className={styles.filters}>
 			<Button button={
@@ -27,6 +55,10 @@ export default function Filters() {
 						{ value: '4', label: '4K' },
 						{ value: '7', label: '7K' },
 					]}></Filter>
+					<Filter filter_key={'difficulty_min'} options={optionsDiffMin}></Filter>
+					<Filter filter_key={'difficulty_max'} options={optionsDiffMax}></Filter>
+					<Filter filter_key={'year_min'} options={optionsYearMin}></Filter>
+					<Filter filter_key={'year_max'} options={optionsYearMax}></Filter>
 				</div>
 			}
 		</div>
